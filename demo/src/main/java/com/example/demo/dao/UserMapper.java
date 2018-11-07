4c4d9f0708b42c2cde37da671d737572f7afb746
明章永（数据服务产品部） <mingzhangyong@egova.com.cn>
2018-11-07 16:55:53 +0800

    spring boot + mybatis

---
+++ demo/src/main/java/com/example/demo/dao/UserMapper.java
@@ -0,0 +1,23 @@
+package com.example.demo.dao;
+
+
+
+
+import com.example.demo.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+@Mapper
+public interface UserMapper {
+
+/*    List<User> findUserByName(String name);
+
+    public List<User> ListUser();*/
+
+    public int insertUser(User user);
+
+    public int delete(int id);
+
+    public int Update(User user);
+
+}
---
+++ demo/src/main/java/com/example/demo/service/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+/*
+package com.example.demo.service.serviceImpl;
+
+
+
+import com.example.demo.dao.UserMapper;
+import com.example.demo.pojo.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+*/
+/**
+ * Created by toutou on 2018/9/15.
+ *//*
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserMapper userMapper;
+
+    @Override
+    public List<User> getUser(int age){
+        return userMapper.getUser(age);
+    }
+
+    @Override
+    public boolean insertUsers(String id, String psw) {
+        return userMapper.insertUsers(id,psw);
+    }
+}
+*/
